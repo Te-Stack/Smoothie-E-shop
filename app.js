fb5346baf4d48804186fa7ed70a08ea9143b980f
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2021-01-03 14:14:36 +0100

    smoothie sale website

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require("dotenv")
+const cors = require("cors")
+const cookieParser = require("cookie-parser")
+const authRoutes = require("./routes/authroutes");
+const { requireAuth, checkUser } = require('./middleware/authMiddleware');
+
+const app = express();
+
+//dotenv config
+dotenv.config()
+
+// middleware
+app.use(express.static('public'));
+app.use(express.json())
+app.use(cookieParser())
+
+//allow cross origin requests 
+app.use(cors())
+// view engine
+app.set('view engine', 'ejs');
+ 
+// database connection
+mongoose.connect(process.env.DBURI, { useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex:true})
+  
+const port = 3000 || process.env.PORT
+
+app.listen(port,()=>{
+    console.log(`server running in port'${port}`)
+})
+
+// routes
+app.get("*",checkUser)
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies', requireAuth ,(req, res) => res.render('smoothies'));
+app.use(authRoutes)
